10d43b250df329be24c30528b05e049553efd542
Yassin97440 <yassin.abdulla@compufirst.com>
2025-01-04 03:42:02 +0100

    TIMELINE cursus WIP : pb affichage last item

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="p-6">
+  <div class="px-6 grid grid-flow-row ">
     <h1 class="text-3xl font-bold mb-4">Mon Parcours</h1>
 
-    <h2 class="text-xl font-semibold mt-6 mb-2">Mon Cursus Scolaire</h2>
-    <div v-for="(edu, index) in education" :key="index" class="mb-4">
-      <p class="font-bold">{{ edu.degree }} - {{ edu.school }}</p>
-      <p>{{ edu.dates }}</p>
-      <p class="text-sm">{{ edu.description }}</p>
-    </div>
 
     <Card class="w-full bg-background rounded-lg ">
       <template #title class="">
@@ -40,6 +34,28 @@
       </template>
     </Card>
   </div>
+  <Card>
+    <template #title>
+      <h2 class="text-xl font-semibold mt-6 mb-2">Mon Cursus Scolaire</h2>
+    </template>
+    <template #content>
+      <Timeline :value="education" layout="horizontal" align="alternate"
+        class="horizontal-timeline w-full grid-flow-col">
+        <template #marker="slotProps">
+          <span class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm">
+            <i class="pi pi-book"></i>
+          </span>
+        </template>
+        <template #opposite="slotProps">
+          {{ slotProps.item.dates }}
+        </template>
+        <template #content="slotProps">
+          <p class="text-secondary">{{ slotProps.item.school }}</p>
+          <p>{{ slotProps.item.degree }}</p>
+        </template>
+      </Timeline>
+    </template>
+  </Card>
 </template>
 
 <script setup lang="ts">
@@ -47,3 +63,5 @@ import { usePortfolioStore } from "~/stores/portfolio";
 const store = usePortfolioStore();
 const { experiences, education } = store;
 </script>
+
+<style scoped></style>
