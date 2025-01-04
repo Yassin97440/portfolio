002ec2f00abf84ba4134de100f4e44d04aeaa277
Yassin97440 <yassin.abdulla@compufirst.com>
2025-01-04 22:11:59 +0100

    layout OK

--- app/layouts/Default.vue
+++ app/layouts/Default.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="min-h-screen text-text relative">
-    <MenuBar :model="items" class="bg-primary  relative z-20">
+    <MenuBar class="bg-primary  relative z-20 ">
       <template #start>
         <h1 class="text-2xl font-bold">Yassin Abdulla</h1>
       </template>
-      <template #item="{ item, props, hasSubmenu, root }">
-        <a v-ripple class="flex items-center" v-bind="props.action">
-          <span class="text-action">{{ item.label }}</span>
-        </a>
+      <!-- <template #item="{ item, props, hasSubmenu, root }">
+        <div class="w-full">
+
+          <a v-ripple class="flex items-center " v-bind="props.action">
+            <span class="text-action">{{ item.label }}</span>
+          </a>
+        </div>
+      </template> -->
+      <template #end>
+        <div class="flex flex-row ">
+          <Button v-for="item in items" :key="item.label" class=" p-2 mx-2 hover:bg-#f3f4f6">
+            <NuxtLink :to="item.to" class="text-action">{{ item.label }}</NuxtLink>
+          </Button>
+        </div>
       </template>
     </MenuBar>
     <div class="fixed inset-0 w-full h-full">
@@ -21,7 +31,7 @@
 
 <script lang="ts" setup>
 const items = [
-  { label: 'Mes projets', icon: 'pi pi-home', to: '/' },
+  // { label: 'Mes projets', icon: 'pi pi-home', to: '/' },
   { label: 'Me contacter', icon: 'pi pi-envelope', to: '/contact' },
 ];
 </script>
