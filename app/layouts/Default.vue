05e13889afb2afca3ad6fa7925e9f6aabf27bb15
Yassin <yassin.abdulla@compufirst.com>
2025-01-06 17:00:49 +0100

    fix responsive display card

--- app/layouts/Default.vue
+++ app/layouts/Default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen text-text relative">
-    <MenuBar class="bg-primary  relative z-20 ">
+    <Toolbar class="bg-primary min-w-screen relative z-20">
       <template #start>
         <h1 class="ml-4 text-2xl font-bold">Yassin Abdulla</h1>
       </template>
@@ -13,15 +13,25 @@
         </div>
       </template> -->
       <template #end>
-        <div class="flex flex-row ">
-          <Button v-for="item in items" :key="item.label" class=" p-2 mx-2 hover:bg-slate-300">
-            <NuxtLink :to="item.to" class="text-action">{{ item.label }}</NuxtLink>
+        <div class="flex flex-row">
+          <Button
+            v-for="item in items"
+            :key="item.label"
+            class="p-2 mx-2 hover:bg-slate-300"
+          >
+            <NuxtLink :to="item.to" class="text-action">{{
+              item.label
+            }}</NuxtLink>
           </Button>
         </div>
       </template>
-    </MenuBar>
+    </Toolbar>
     <div class="fixed inset-0 w-full h-full">
-      <img class="w-full h-full object-cover" src="~/assets/bgw.webp" alt="Background" />
+      <img
+        class="w-full h-full object-cover"
+        src="~/assets/bgw.webp"
+        alt="Background"
+      />
     </div>
     <main class="relative z-10 h-screen">
       <slot />
@@ -30,9 +40,11 @@
 </template>
 
 <script lang="ts" setup>
+import Toolbar from "primevue/toolbar";
+
 const items = [
   // { label: 'Mes projets', icon: 'pi pi-home', to: '/' },
-  { label: 'Me contacter', icon: 'pi pi-envelope', to: '/contact' },
+  { label: "Me contacter", icon: "pi pi-envelope", to: "/contact" },
 ];
 </script>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,68 +1,98 @@
 <template>
-  <div class="p-5 md:p-14 lg:p-20 grid grid-flow-row ">
-
-
-
-    <Card class="w-full bg-background rounded-lg mb-20">
+  <div class="p-5 md:p-14 lg:p-20">
+    <!-- experiences pro -->
+    <Card class="bg-background rounded-lg mb-20">
       <template #title class="">
-        <h2 class="text-2xl ">Mes Expériences Professionnelles</h2>
+        <h2 class="text-2xl">Mes Expériences Professionnelles</h2>
       </template>
       <template #content>
-        <Timeline :value="experiences" :align="width >= 768 ? 'alternate' : 'bottom'" class="">
+        <Timeline
+          :value="experiences"
+          :align="width >= 768 ? 'alternate' : 'bottom'"
+          class=""
+        >
           <template #marker="slotProps">
-            <span class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm">
+            <span
+              class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm"
+            >
               <i class="pi pi-check"></i>
             </span>
           </template>
           <template #opposite="slotProps">
-            <p class="text-secondary sm hidden md:block">{{ slotProps.item.dates }}</p>
+            <p class="text-secondary sm hidden md:block">
+              {{ slotProps.item.dates }}
+            </p>
           </template>
           <template #content="slotProps">
-            <Card class="my-4 bg-primary">
-              <template #title>
-                {{ slotProps.item.title }}
-              </template>
-              <template #subtitle>
-                <p class="text-secondary">{{ slotProps.item.company }}</p>
-              </template>
-              <template #content class="">
-                <div class="">
-
-                  <ul class="relative list-disc ml-3">
-                    <li v-for="detail in slotProps.item.Details" :key="detail" class="text-left">
-                      {{ detail }}
-                    </li>
-                    <Button class="text-action text-sm absolute left-0" label="Lire plus" text
-                      @click="navigateTo(`/experience/${slotProps.item.id}`)">
-                    </Button>
-                  </ul>
-                </div>
-              </template>
-            </Card>
+            <DeferredContent :loading="true">
+              <Card class="my-4 bg-primary">
+                <template #title>
+                  {{ slotProps.item.title }}
+                </template>
+                <template #subtitle>
+                  <p class="text-secondary">{{ slotProps.item.company }}</p>
+                </template>
+                <template #content class="">
+                  <div class="">
+                    <ul class="relative list-disc ml-3">
+                      <li
+                        v-for="detail in slotProps.item.Details"
+                        :key="detail"
+                        class="text-left"
+                      >
+                        {{ detail }}
+                      </li>
+                      <Button
+                        class="text-action text-sm absolute left-0"
+                        label="Lire plus"
+                        text
+                        @click="navigateTo(`/experience/${slotProps.item.id}`)"
+                      >
+                      </Button>
+                    </ul>
+                  </div>
+                </template>
+              </Card>
+            </DeferredContent>
           </template>
         </Timeline>
       </template>
     </Card>
+
+    <!-- cursus scolaire -->
     <Card class="w-full bg-background rounded-lg">
       <template #title>
         <h2 class="text-xl font-semibold mt-6 mb-2">Mon Cursus Scolaire</h2>
       </template>
       <template #content>
-        <Timeline :value="education" layout="horizontal" align="top" class="horizontal-timeline w-full grid-flow-col">
+        <Timeline
+          :value="education"
+          layout="horizontal"
+          align="top"
+          class="horizontal-timeline w-full grid-flow-col"
+        >
           <template #marker="slotProps">
-            <span class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm">
+            <span
+              class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm"
+            >
               <i class="pi pi-book"></i>
             </span>
           </template>
           <template #opposite="slotProps">
-            <p class="text-secondary ">{{ slotProps.item.dates }}</p>
+            <p class="text-secondary">{{ slotProps.item.dates }}</p>
           </template>
           <template #content="slotProps">
-            <p v-tooltip.top="{
-              value: slotProps.item.school + ' - ' + slotProps.item.shortDescription,
-              showDelay: 800,
-
-            }">{{ slotProps.item.degree }}</p>
+            <p
+              v-tooltip.top="{
+                value:
+                  slotProps.item.school +
+                  ' - ' +
+                  slotProps.item.shortDescription,
+                showDelay: 800,
+              }"
+            >
+              {{ slotProps.item.degree }}
+            </p>
           </template>
         </Timeline>
       </template>
@@ -72,7 +102,7 @@
 
 <script setup lang="ts">
 import { usePortfolioStore } from "~/stores/portfolio";
-import { useWindowSize } from '@vueuse/core'
+import { useWindowSize } from "@vueuse/core";
 
 const { width } = useWindowSize();
 const store = usePortfolioStore();
