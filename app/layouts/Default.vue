36ef76e592550b0106a8c24421b419eb6ad576bc
Yassin97440 <yassin.abdulla@compufirst.com>
2025-01-06 23:03:09 +0100

    add myself section

--- app/layouts/Default.vue
+++ app/layouts/Default.vue
@@ -14,11 +14,7 @@
       </template> -->
       <template #end>
         <div class="flex flex-row">
-          <Button
-            v-for="item in items"
-            :key="item.label"
-            class="p-2 mx-2 hover:bg-slate-300"
-          >
+          <Button v-for="item in items" :key="item.label" class="p-2 mx-2 hover:bg-slate-300">
             <NuxtLink :to="item.to" class="text-action">{{
               item.label
             }}</NuxtLink>
@@ -27,11 +23,7 @@
       </template>
     </Toolbar>
     <div class="fixed inset-0 w-full h-full">
-      <img
-        class="w-full h-full object-cover"
-        src="~/assets/bgw.webp"
-        alt="Background"
-      />
+      <img class="w-full h-full object-cover" src="~/assets/bgw.webp" alt="Background" />
     </div>
     <main class="relative z-10 h-screen">
       <slot />
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,52 @@
 <template>
   <div class="p-5 md:p-14 lg:p-20">
+    <Card class=" bg-background rounded-lg mb-20 ">
+      <template #title>
+        <h2 class="text-2xl">Qui suis-je ?</h2>
+      </template>
+      <template #content>
+        <div class="bg-primary mx-5 rounded-lg p-4">
+          <h3 class="text-xl mb-2">Dans les grosss lignes</h3>
+          <p class="text-sm space-y-2">
+            <span class="block indent-4">
+              Salut, moi c'est Yassin, j'ai 24 ans et je suis développeur sympa 😄. <br>
+              J'ai eu la chance de grandir dans l'océan indien, majoritairement à l'île de la réunion. Avant de venir
+              sur
+              le continent pour continuer mes études et pour découvrir le monde.
+            </span>
+            <span class="block">
+              Cela fait maintenant 4 ans que je suis développeur dans le monde du travail. Dont une année en alternance.
+              Avant cela, j'ai suivi un cursus orienté éléctronique et IoT. J'ai voulu développer mes compétences en
+              développement d'intérface graphique et web afin d'avoir un "spectre" de compétences plus large.
+              Ce qui dans l'idée m'aurait permis d'avoir les compétences nécessaires pour concevoir et développer un
+              projet de A à Z. Qu'il soit purement logiciel ou bien en intégrant des systèmes embarqués.
+            </span>
+            <span class="block">
+              Mais j'avoue que j'ai beaucoup trop kiffé le développement logiciel et web,
+              et j'ai un peu délaissé l'éléctronique ces 3 dernières années.
+              Ce qui m'a permis de découvrir et d'approfondir des notions en développement d'application. De la
+              conception et la mise en place d'une base de données,
+              au développement d'interfaces graphiques, en passant par le développement du back-end.
+              Pendant ma première année je me suis rendu compte que les compétences techniques n'étaient que la moitié
+              des compétences nécessaires pour être un bon développeur.
+              J'ai au fur et à mesure pu découvrir ce qu'est l'analyse du besoin client et son importance.
+              Et peu plus tard j'a eu la chance de voir d'autres problématiques de gestion de projet et la gestion
+              d'équipe....
+            </span>
+          </p>
+        </div>
+      </template>
+
+    </Card>
     <!-- experiences pro -->
     <Card class="bg-background rounded-lg mb-20">
       <template #title class="">
         <h2 class="text-2xl">Mes Expériences Professionnelles</h2>
       </template>
       <template #content>
-        <Timeline
-          :value="experiences"
-          :align="width >= 768 ? 'alternate' : 'bottom'"
-          class=""
-        >
+        <Timeline :value="experiences" :align="width >= 768 ? 'alternate' : 'bottom'" class="">
           <template #marker="slotProps">
-            <span
-              class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm"
-            >
+            <span class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm">
               <i class="pi pi-check"></i>
             </span>
           </template>
@@ -24,7 +56,7 @@
             </p>
           </template>
           <template #content="slotProps">
-            <DeferredContent :loading="true">
+            <DeferredContent>
               <Card class="my-4 bg-primary">
                 <template #title>
                   {{ slotProps.item.title }}
@@ -35,19 +67,11 @@
                 <template #content class="">
                   <div class="">
                     <ul class="relative list-disc ml-3">
-                      <li
-                        v-for="detail in slotProps.item.Details"
-                        :key="detail"
-                        class="text-left"
-                      >
+                      <li v-for="detail in slotProps.item.Details" :key="detail" class="text-left">
                         {{ detail }}
                       </li>
-                      <Button
-                        class="text-action text-sm absolute left-0"
-                        label="Lire plus"
-                        text
-                        @click="navigateTo(`/experience/${slotProps.item.id}`)"
-                      >
+                      <Button class="text-action text-sm absolute left-0" label="Lire plus" text
+                        @click="navigateTo(`/experience/${slotProps.item.id}`)">
                       </Button>
                     </ul>
                   </div>
@@ -65,16 +89,9 @@
         <h2 class="text-xl font-semibold mt-6 mb-2">Mon Cursus Scolaire</h2>
       </template>
       <template #content>
-        <Timeline
-          :value="education"
-          layout="horizontal"
-          align="top"
-          class="horizontal-timeline w-full grid-flow-col"
-        >
+        <Timeline :value="education" layout="horizontal" align="top" class="horizontal-timeline w-full grid-flow-col">
           <template #marker="slotProps">
-            <span
-              class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm"
-            >
+            <span class="bg-secondary flex w-8 h-8 items-center justify-center rounded-full z-10 shadow-sm">
               <i class="pi pi-book"></i>
             </span>
           </template>
@@ -82,15 +99,13 @@
             <p class="text-secondary">{{ slotProps.item.dates }}</p>
           </template>
           <template #content="slotProps">
-            <p
-              v-tooltip.top="{
-                value:
-                  slotProps.item.school +
-                  ' - ' +
-                  slotProps.item.shortDescription,
-                showDelay: 800,
-              }"
-            >
+            <p v-tooltip.top="{
+              value:
+                slotProps.item.school +
+                ' - ' +
+                slotProps.item.shortDescription,
+              showDelay: 800,
+            }">
               {{ slotProps.item.degree }}
             </p>
           </template>
