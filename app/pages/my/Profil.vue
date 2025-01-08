eb0f3bb911c29bc38a7b63bf721624835b5c8e16
Yassin <yassin.abdulla@compufirst.com>
2025-01-08 17:51:25 +0100

    MyProfil page : WIP

--- app/pages/my/Profil.vue
+++ app/pages/my/Profil.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    
+  <div class="p-5 md:p-14 lg:p-28">
+    <Card class="bg-background rounded-lg mb-20">
+      <template #header>
+        <h2 class="text-2xl">Mon profil</h2>
+      </template>
+    </Card>
+    <Card class="bg-background rounded-lg mb-20">
+      <template #header>
+        <h2 class="text-3xl font-bold mb-4">Mes compétences</h2>
+      </template>
+    </Card>
+    <Card class="bg-background rounded-lg mb-20">
+      <template #header>
+        <h2 class="text-3xl font-bold mb-4">Mes technologies</h2>
+      </template>
+    </Card>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { usePortfolioStore } from "~/stores/portfolio";
+const portfolioStore = usePortfolioStore();
 
+const myselfData = portfolioStore.myself;
 </script>
 
-<style>
-
-</style>
+<style></style>
--- app/stores/portfolio.ts
+++ app/stores/portfolio.ts
@@ -1,11 +1,12 @@
 import { defineStore } from 'pinia'
 import experiences from '~/data/experiences.json'
 import education from '~/data/education.json'
-
+import myself from '~/data/myself.json'
 export const usePortfolioStore = defineStore('portfolio', {
     state: () => ({
         experiences,
-        education
+        education,
+        myself
     }),
     getters: {
         getExperienceById: (state) => (id: number) => state.experiences.find((exp) => exp.id === id)
