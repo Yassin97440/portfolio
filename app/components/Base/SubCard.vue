b7d7fbefb10ea29d5b6c56ed9c505df499854eb5
Yassin <yassin.abdulla@compufirst.com>
2025-01-09 17:56:33 +0100

    Add new Card and SubCard components; update Profil page and myself data
    - Created `Card.vue` and `SubCard.vue` components for better UI structure.
    - Updated `Profil.vue` to utilize new components and improved layout.
    - Enhanced `myself.json` with additional technology and tool information.

---
+++ app/components/Base/SubCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-primary mx-5 rounded-lg p-4">
+    <h3 class="text-xl mb-2">
+      <slot name="title">{{ title }}</slot>
+    </h3>
+
+    <div class="content">
+      <slot></slot>
+    </div>
+
+    <div class="footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title?: string;
+}>();
+</script>
+
+<style></style>
--- app/pages/my/Profil.vue
+++ app/pages/my/Profil.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="p-5 md:p-14 lg:p-28">
     <Card class="bg-background rounded-lg mb-20">
-      <template #header>
-        <h2 class="text-2xl">Mon profil</h2>
+      <template #title>
+        <h2 class="text-xl">Mon profil</h2>
+      </template>
+      <template #content>
+        <div class="bg-primary mx-5 rounded-lg p-4">
+          <h3 class="text-xl mb-2">Je = Moi</h3>
+          <p class="text-sm space-y-2">
+            <span class="block indent-4">
+              Salut, moi c'est Yassin, j'ai 24 ans et je suis un développeur
+              sympa 😄
+            </span>
+            <span class="block"> </span>
+            <span class="block"> </span>
+          </p>
+        </div>
       </template>
     </Card>
-    <Card class="bg-background rounded-lg mb-20">
-      <template #header>
-        <h2 class="text-3xl font-bold mb-4">Mes compétences</h2>
+    <Card class="bg-background rounded-lg mb-20 pt-1">
+      <template #title>
+        <h2 class="text-2xl">Mes compétences</h2>
+      </template>
+      <template #content>
+        <div class="bg-primary mx-5 rounded-lg p-4">
+          <h3 class="text-xl mb-2">Je = Moi</h3>
+        </div>
       </template>
     </Card>
-    <Card class="bg-background rounded-lg mb-20">
-      <template #header>
-        <h2 class="text-3xl font-bold mb-4">Mes technologies</h2>
+    <Card class="bg-background rounded-lg mb-20 pt-1">
+      <template #title>
+        <h2 class="text-2xl">Mes technologies</h2>
+      </template>
+      <template #content>
+        <BaseSubCard title="TEST">
+          <template #content>
+            <p>TEST</p>
+          </template>
+        </BaseSubCard>
       </template>
     </Card>
   </div>
