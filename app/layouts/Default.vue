dedbca424431dc5b69aea6906dfdfa90104fe318
Yassin <yassin.abdulla@compufirst.com>
2025-01-10 11:23:12 +0100

    better bg app and correction faute

--- app/layouts/Default.vue
+++ app/layouts/Default.vue
@@ -2,11 +2,17 @@
   <div class="min-h-screen text-text relative">
     <MenuBar class="bg-primary min-w-screen relative z-20">
       <template #start>
-        <h1 class="ml-4 text-2xl font-bold" @click="navigateTo('/')">Yassin Abdulla</h1>
+        <h1 class="ml-4 text-2xl font-bold" @click="navigateTo('/')">
+          Yassin Abdulla
+        </h1>
       </template>
       <template #end>
         <div class="flex flex-row">
-          <Button v-for="item in items" :key="item.label" class="p-2 mx-2 hover:bg-slate-300">
+          <Button
+            v-for="item in items"
+            :key="item.label"
+            class="p-2 mx-2 hover:bg-slate-300"
+          >
             <NuxtLink :to="item.to" class="text-action">{{
               item.label
             }}</NuxtLink>
@@ -15,7 +21,11 @@
       </template>
     </MenuBar>
     <div class="fixed inset-0 w-full h-full">
-      <img class="w-full h-full object-cover" src="~/assets/bgw.webp" alt="Background" />
+      <img
+        class="w-full h-full object-cover blur-md brightness-95"
+        src="~/assets/bgw.webp"
+        alt="Background"
+      />
     </div>
     <main class="relative z-10 h-screen">
       <slot />
