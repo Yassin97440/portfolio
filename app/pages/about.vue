6ba3d8ffa1e19f63fda4357789303bc40dc19982
Yassin97440 <yassin.abdulla@compufirst.com>
2025-01-19 17:26:53 +0100

    update my profil page and about page

---
+++ app/pages/about.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+
+
+// Métadonnées spécifiques à la page
+useSeoMeta({
+    title: 'À propos - Mon Site',
+    description: 'Découvrez mon parcours, mes compétences et ma passion pour le développement web',
+})
+
+const skills = {
+
+    tools: [
+        'Git',
+        'Docker',
+        'CI/CD',
+        'Jira',
+    ]
+}
+
+// const openUrl = (url: string) => {
+//     if (typeof window !== 'undefined') {
+//         window.open(url, '_blank')
+//     }
+// }
+</script>
+
+<template>
+    <div class="max-w-4xl mx-auto px-4 py-8">
+        <Card>
+            <template #title>
+                <div class="flex items-center gap-2"></div>
+            </template>
+            <template #content>
+
+                <BaseSubCard>
+                    <template #title>
+                        <div class="flex items-center gap-2">
+                            <Badge value="01" severity="primary" />
+                            À propos
+                        </div>
+                    </template>
+                    <div class="space-y-8">
+                        <!-- Section Qui suis-je -->
+                        <section>
+                            <h2 class="text-xl font-semibold mb-4">Qui suis-je ?</h2>
+                            <p class="text-lg">
+                                Passionné par le développement web, je crée des solutions numériques innovantes
+                                qui combinent performance technique et expérience utilisateur intuitive.
+                            </p>
+                        </section>
+
+                    </div>
+                </BaseSubCard>
+
+
+                <BaseSubCard>
+                    <template #title>
+                        <h2 class="text-xl font-semibold mb-4">Mon approche</h2>
+
+                    </template>
+                    <!-- Section Approche -->
+                    <section>
+                        <p class="text-lg">
+                            Je crois en la création de solutions web robustes et évolutives,
+                            en utilisant les meilleures pratiques et les technologies modernes.
+                            Mon objectif est de développer des applications qui non seulement
+                            répondent aux besoins actuels mais anticipent aussi les évolutions futures.
+                        </p>
+                    </section>
+                </BaseSubCard>
+                <BaseSubCard>
+                    <template #title>
+
+                    </template>
+                    <!-- Section GitHub -->
+                    <h2 class="text-xl font-semibold mb-4">Me retrouver</h2>
+                    <!-- <Button icon="pi pi-github" label="Voir mon GitHub" severity="secondary" link
+                                    @click="openUrl('https://github.com/VOTRE-USERNAME')" /> -->
+                </BaseSubCard>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<style scoped>
+.bg-surface-0 {
+    background-color: var(--surface-0);
+}
+</style>
--- app/pages/my/Profil.vue
+++ app/pages/my/Profil.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="p-5 md:p-14 lg:p-28">
+  <div class="p-5 md:p-14 lg:px-24 lg:py-16">
     <main>
-      <section class="mb-12">
+      <section class="mb-8 md:mb-12">
         <h1 class="text-3xl font-bold mb-4">Mon Profil</h1>
         <p class="text-xl text-gray-600">Développeur passionné par l'innovation</p>
       </section>
@@ -12,18 +12,20 @@
             <article class="space-y-8 p-6">
               <div class="prose max-w-none">
                 <h2 class="text-2xl font-semibold mb-4">Qui suis-je ?</h2>
-                <p class="leading-relaxed">
+                <p class="">
                   {{ myselfData.longDescription1 }}
                 </p>
               </div>
 
               <div class="prose max-w-none">
                 <h2 class="text-2xl font-semibold mb-4">Mon Parcours</h2>
-                <p class="leading-relaxed">
-                  Passionné par la technologie depuis mon plus jeune âge, j'ai commencé avec l'électronique et l'IoT au
-                  lycée.
-                  Mon BTS Système numérique m'a permis d'approfondir ces connaissances, avant de me spécialiser dans le
-                  développement logiciel.
+                <p class="">
+                  J'ai toujours aimé résoudre des problèmes et aussi bidouiller sur l'ordinateur. Je me suis dès le
+                  début orienté
+                  dans le domaine de l'informatique et des systèmes numériques.Ca me parassait être une évidence. <br>
+                  Mes dernières années de lycée et mon BTS Système numérique m'ont permis d'approfondir de décourvir et
+                  d'appronfir des notions assez "bas niveau" dans les systèmes numériques, avant de me spécialiser dans
+                  le développement logiciel et web.
                 </p>
               </div>
 
@@ -31,17 +33,26 @@
                 <h2 class="text-2xl font-semibold mb-4">Expériences dans le développement logiciel</h2>
                 <ul class="space-y-4">
                   <li>
-                    <h3 class="text-xl font-medium mb-2">Lead Développeur</h3>
-                    <p class="leading-relaxed">
-                      Direction technique d'une équipe sur un ERP complexe, gestion de projets et développement
-                      full-stack.
-                    </p>
-                  </li>
-                  <li>
-                    <h3 class="text-xl font-medium mb-2">Développeur Full Stack</h3>
-                    <p class="leading-relaxed">
-                      Développement back-end et front-end, participation à la conception et à l'amélioration continue
-                      des solutions de l'entreprise. Principalement l'EPR. Avec une stack full java.
+                    <h3 class="text-xl font-medium mb-2">Après cette expérience</h3>
+                    <p class="">
+                      Aujourd'hui, je pense que je suis loin d'avoir vu 1/4 des problèmatiques qui peuvent être
+                      rencontrés dans le domaine du développement logiciel.
+                      Mais j'ai pu traiter tellement de demandes et problèmatiques, touchant au développement logiciel,
+                      à la digitalisation d'une entreprise, sa stratégie vis à vis du digital. <br>
+                      J'ai pu apprendre à développer un logiciel de la base de données à l'interface utilisateur à
+                      partir d'un besoin.
+                      J'ai pu apprendre à gérer des équipes de développeurs, à gérer des projets, développer avec des
+                      délais très serrés (et très larges aussi). <br>
+                      J'ai appris à prioriser les demandes et les problèmatiques en prenant en compte les enjeux
+                      business et les contraintes techniques. <br>
+                      J'ai appris à commuiquer avec mes clients, avec mon équipe en tant que développeur, mais aussi en
+                      tant que lead dév, chef de projet, et d'équipe.
+                      Et également avec des équipes éxternes. <br>
+                      Tout ça bien sur en faisant des erreurs, en essayant, en allant chercher. <br>
+                      Ce que je peux dire que j'ai étais bien accompagné avec une super équipe, ce qui m'a permit
+                      d'évoluer en autonomie sur pleins de nouveaux sujets.
+                      Je parle plus en détailx
+
                     </p>
                   </li>
                 </ul>
@@ -59,7 +70,8 @@
           <template #content>
             <BaseSubCard>
               <div class="flex flex-wrap gap-3 p-4">
-                <Chip v-for="skill in myselfData.skills" :key="skill" class="bg-secondary text-white px-4 py-2">
+                <Chip v-for="skill in myselfData.skills.otherSkills" :key="skill"
+                  class="bg-secondary text-white px-4 py-2">
                   {{ skill }}
                 </Chip>
               </div>
@@ -68,18 +80,43 @@
         </Card>
       </section>
 
-      <section class="mb-12">
+      <section class="">
         <Card class="bg-background rounded-lg">
           <template #title>
             <h2 class="text-2xl font-semibold">Technologies</h2>
           </template>
           <template #content>
+
             <BaseSubCard>
-              <div class="flex flex-wrap gap-3 p-4">
-                <Chip v-for="tech in myselfData.technos" :key="tech" class="bg-secondary text-white px-4 py-2">
-                  {{ tech }}
-                </Chip>
-              </div>
+              <template #title>
+                <h2 class="text-xl font-semibold mb-4">Mes technologies</h2>
+
+              </template>
+              <section>
+                <!-- Section Compétences -->
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                  <div class="p-4 border rounded-lg bg-surface-0">
+                    <h3 class="font-medium mb-4 capitalize">frontend</h3>
+                    <ul class="list-none p-0 m-0">
+                      <li v-for="skill in myselfData.skills.technos.frontend" :key="skill"
+                        class="mb-2 flex items-center">
+                        <i class="pi pi-check text-green-500 mr-2"></i>
+                        {{ skill }}
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="p-4 border rounded-lg bg-surface-0">
+                    <h3 class="font-medium mb-4 capitalize">Backend</h3>
+                    <ul class="list-none p-0 m-0">
+                      <li v-for="skill in myselfData.skills.technos.backend" :key="skill"
+                        class="mb-2 flex items-center">
+                        <i class="pi pi-check text-green-500 mr-2"></i>
+                        {{ skill }}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </section>
             </BaseSubCard>
           </template>
         </Card>
@@ -94,6 +131,8 @@ import { usePortfolioStore } from "~/stores/portfolio";
 const portfolioStore = usePortfolioStore();
 const { myselfData } = portfolioStore;
 
+
+
 useSeoMeta({
   title: 'Mon Profil - Développeur logiciel',
   description: 'Découvrez mon parcours de développeur full stack, mes compétences techniques et mon expérience professionnelle dans le développement web et logiciel.',
@@ -105,5 +144,15 @@ useSeoMeta({
 <style>
 .prose {
   @apply text-base md:text-lg;
+}
+
+/* Ajout d'une règle CSS native plus performante */
+p {
+  line-height: 1.6;
+}
+
+/* On peut aussi cibler spécifiquement les paragraphes de la prose */
+.prose p {
+  line-height: 1.6;
 }
 </style>
