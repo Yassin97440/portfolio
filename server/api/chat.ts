7e0b2e8d366b2234ecd0f6d723ae4228ecdd19d8
Yassin97440 <yassin.abdulla@compufirst.com>
2025-02-27 14:03:14 +0100

    return good type after debug

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -29,15 +29,15 @@ export default defineEventHandler(async (event) => {
         const chatResponse: ChatCompletionResponse = await client.chat.complete({
             model: 'mistral-large-latest',
             messages: chatBotMessages,
-        });
+        })
 
         if (!chatResponse.choices?.length) {
-            throw new Error('No response from chat API');
+            throw new Error('No response from chat API')
         }
-        return { 'message': chatResponse.choices[0].message.content, 'apiKey ': process.env.MISTRAL_API_KEY };
+        return chatResponse.choices[0].message.content
     }
     catch (error) {
 
-        return { error, 'apiKey ': process.env.MISTRAL_API_KEY };
+        return error
     }
 }) 
