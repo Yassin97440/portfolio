3d099d0ccb064834c6a171e3757d721f825dd63b
Yassin97440 <yassin.abdulla@compufirst.com>
2025-02-27 11:36:22 +0100

    try fix unauthorized error from nuxt server

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,8 +1,23 @@
-import { defineEventHandler, readBody } from 'h3'
+import { defineEventHandler, readBody, H3Event } from 'h3'
 import { Mistral } from '@mistralai/mistralai';
 import { ChatCompletionResponse } from '@mistralai/mistralai/models/components';
 
+const handleRequest = (event: H3Event) => {
+    // Configurer les headers CORS
+    setResponseHeaders(event, {
+        'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*'
+    })
+
+    // Gérer les requêtes OPTIONS (preflight)
+    if (event.method === 'OPTIONS') {
+        return 'OK'
+    }
+}
 export default defineEventHandler(async (event) => {
+    // Configurer les headers CORS
+    handleRequest(event)
     const chatBotMessages = await readBody(event)
 
     const apiKey = process.env.MISTRAL_API_KEY;
