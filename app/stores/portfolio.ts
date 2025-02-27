539609233e0bb726b2c9549ba676da5166258009
Yassin97440 <yassin.abdulla@compufirst.com>
2025-02-27 21:57:59 +0100

    restructure projects data

--- app/stores/portfolio.ts
+++ app/stores/portfolio.ts
@@ -2,11 +2,13 @@ import { defineStore } from 'pinia'
 import experiences from '~/data/experiences.json'
 import education from '~/data/education.json'
 import myselfData from '~/data/myselfData.json'
+import projects from '~/data/projects.json'
 export const usePortfolioStore = defineStore('portfolio', {
     state: () => ({
         experiences,
         education,
-        myselfData
+        myselfData,
+        projects
     }),
     getters: {
         getExperienceById: (state) => (id: number) => state.experiences.find((exp) => exp.id === id)
