9e0993109a24eeda7298f9dc8826f44571cdb1fb
Yassin97440 <yassinabdulla614@gmail.com>
2025-05-01 22:30:12 +0200

    use .env for strapi db config

--- strapi-portfolio/config/database.ts
+++ strapi-portfolio/config/database.ts
@@ -4,9 +4,9 @@ export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
-    mysql: {
+    postgres: {
       connection: {
-        host: env('DATABASE_HOST', '82.29.184.26'),
+        host: env('DATABASE_HOST', ''),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi_db'),
         user: env('DATABASE_USERNAME', 'strapi'),
@@ -22,7 +22,7 @@ export default ({ env }) => {
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-    postgres: {
+    mysql: {
       connection: {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
