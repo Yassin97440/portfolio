259fe4ab5091073bcd05874bad88cb88eaba9185
Yassin97440 <yassinabdulla614@gmail.com>
2025-08-30 16:31:30 +0400

    improve chatbot for my services

---
+++ app/data/services.ts
@@ -0,0 +1,66 @@
+const services = [
+    {
+        "title": "Audit et conseil",
+        "description": "Analyse approfondie de votre système existant. Ainsi que vos besoins actuels et futurs. Tout en prenant en compte vos flux métiers et contexte global afin de vous proposer les meilleures solutions.",
+        "features": [
+            "Consulting",
+            "Audit complet de votre SI, de vos flux métiers",
+            "Conseil architecture",
+        ]
+    },
+    {
+        "title": "Développement web fullstack",
+        "description": "Création d'applications web complètes avec les dernières technologies.",
+        "features": [
+            "Consulting",
+            "Développement web fullstack",
+        ]
+    },
+    {
+        "title": "Solutions sur-mesure",
+        "description": "Solution complète pour lancer votre produit rapidement",
+        "features": [
+            "Consulting",
+            "Applications web et logiciels (frontend/backend)",
+            "SaaS",
+            "Sites internet (vitrine, e-commerce)",
+            "Progiciels client lourd",
+            "API REST"
+        ]
+    },
+    {
+        "title": "Architecture logicielle & bases de données",
+        "description": "Analyse approfondie de votre système existant",
+        "features": [
+            "Consulting",
+            "Revue de code",
+            "Audit complet de votre SI",
+            "Conseil architecture",
+            "Recommandations détaillées",
+            "Plan d'action",
+            "Rapport complet"
+        ]
+    },
+    {
+        "title": "Accompagnement / CTO freelance",
+        "description": "Intégrez l'intelligence artificielle dans vos processus",
+        "features": [
+            "Consulting",
+            "Intégration support client IA",
+            "Assistant IA personnalisé",
+            "Vector Database",
+        ]
+    },
+    {
+        "title": "IA / Automatisation intelligente",
+        "description": "Intégrez l'intelligence artificielle dans vos processus",
+        "features": [
+            "Consulting",
+            "Intégration support client IA",
+            "Assistant IA personnalisé",
+            "Vector Database",
+        ]
+    }
+]
+
+export default services
--- services/systemprompt.ts
+++ services/systemprompt.ts
@@ -1,7 +1,7 @@
 import myProfil from '~/data/myselfData.json'
 import experiences from '~/data/experiences.json'
-import education from '~/data/education.json'
 import { projects } from '~/data/projects'
+import services from '~/data/services'
 
 const getPertinentData = (): any => {
     const myData: string = myProfil.longDescription2 + myProfil.longDescription3 + myProfil.longDescription4
@@ -15,22 +15,36 @@ const myData = getPertinentData()
 const MY_DATA = {
     myData,
     experiences,
-    education,
     projects,
+    services,
 }
 
 const SYSTEM_PROMPT_LATEST: string = `
-Tu es mon responsable marketing. Ton rôle est de me vendre et de me mettre en avant auprès des visiteurs de mon portfolio, qui sont des recruteurs ou des clients potentiels. Tu es donc un chatbot sur mon portfolio. Fais des réponses courtes et concises.
+Tu es mon responsable marketing et agent de communication. 
+Ton rôle est de vendre mes services, mon expertise et de me mettre en avant auprès des visiteurs de mon site internet, qui sont des clients potentiels. 
+Tu es donc un chatbot sur mon site internet.
 Tes réponses seront affichées dans une petite zone, donc des réponses courtes sont préférables.
-Ci-dessous se trouvent des données au format JSON me concernant, réparties en trois grandes catégories : myProfil, experiences et education. Analyse et enregistre ces données pour pouvoir répondre aux questions des visiteurs de mon portfolio.
-Reformule toujours les informations fournies dans tes réponses. Important : Ne réponds jamais en Markdown, utilise uniquement du texte brut. Sois toujours concis mais pertinent, en fournissant les informations nécessaires sans décourager la lecture.
-Pour les questions générales, reste généraliste. Ne donne des détails précis que si on te les demande. Après tes réponses, poses des questions sur des aspects de mon profil ou de ma présence sur le site qui pourraient intéresser les visiteurs.
-Adopte un ton professionnel mais engageant. Si une question est complexe ou ambiguë, demande des clarifications. Sois prêt à recevoir des retours et à t'améliorer en fonction des interactions avec les utilisateurs.
+Ci-dessous se trouvent des données au format JSON me concernant, réparties en quatre grandes catégories : myProfil, experiences et services. 
+Analyse et enregistre ces données pour pouvoir répondre aux questions des visiteurs de mon portfolio.
+Reformule toujours les informations fournies dans tes réponses.
+Important : Ne réponds jamais en Markdown, utilise uniquement du texte brut.
+Sois toujours concis mais pertinent, en fournissant les informations nécessaires sans décourager la lecture.
+Ne parles pas de moi à la première personne mais à la 3e personne.
+Pour les questions générales, reste généraliste. Ne donne des détails précis que si on te les demande. 
+En fin de réponses, à partir de la question du visiteur, essaie d'intérpéter l'intention de l'utilisateur 
+et de lui poser des questions sur des aspects de mon profil, de mes compétences et/ou de mes services qui pourraient intéresser le visiteur.
+Adopte un ton professionnel mais engageant. Si une question est complexe ou ambiguë, demande des clarifications. 
+Après 6 réponses, propose de me contactez sur ma page contact, pour une discussion plus approfondie.
+Sois prêt à recevoir des retours et à t'améliorer en fonction des interactions avec les utilisateurs.
 
 Merci par avance.
 `
 const SYSTEM_PROMPT_TOTAL: string = `
-${SYSTEM_PROMPT_LATEST} ${JSON.stringify(MY_DATA)}
+${SYSTEM_PROMPT_LATEST} 
+
+======= Données pertinentes : =======
+
+${JSON.stringify(MY_DATA)}
 `
 
 export default SYSTEM_PROMPT_TOTAL
